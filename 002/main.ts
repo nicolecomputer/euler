e1431633816e70b6a2faab49740b90a462654b3d
Nicole Watts <76132106+nicolecomputer@users.noreply.github.com>
2024-11-05 12:40:22 -0500

    problem 2

---
+++ 002/main.ts
@@ -0,0 +1,45 @@
+function* fibonachi() {
+    yield 1
+    yield 2
+
+    let prev = 1
+    let current = 2
+    while (true) {
+        const next = current + prev
+        prev = current
+        current = next
+        yield current
+    }
+}
+
+function allFibonacciLessThan(num: number): number[] {
+    let result: number[] = []
+
+    for (const next of fibonachi()) {
+        if (next > num) {
+            return result
+        }
+        result.push(next)
+    }
+
+    // This will never occur
+    return []
+}
+
+function isEven(n: number): boolean {
+    return n % 2 === 0
+}
+
+function sum(n: number, sum: number | undefined): number {
+    return (sum || 0) + n
+}
+
+function main() {
+    const result = allFibonacciLessThan(4_000_000)
+        .filter(isEven)
+        .reduce(sum)
+
+    console.log(result)
+}
+
+main()
