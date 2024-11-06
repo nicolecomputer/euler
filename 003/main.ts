49639b4753423799968fcffb7b3150c3c4aa631c
Nicole Watts <76132106+nicolecomputer@users.noreply.github.com>
2024-11-06 12:29:20 -0500

    largest prime factor

---
+++ 003/main.ts
@@ -0,0 +1,49 @@
+function isPrime(n: number, knownPrimes: number[]): boolean {
+    if (n == 2) {
+        return true
+    }
+
+    for (const prime of knownPrimes) {
+        if (n % prime == 0) {
+            return false
+        }
+    }
+
+    return true
+}
+
+function* primes() {
+    yield 2
+
+    let knownPrimes: number[] = [2]
+    let currentNumber = 3
+
+    while (true) {
+        if (isPrime(currentNumber, knownPrimes)) {
+            knownPrimes.push(currentNumber)
+            yield currentNumber
+        }
+
+        currentNumber += 1
+    }
+}
+
+function primeFactors(n: number): number[] {
+    for (const prime of primes()) {
+        if (prime > n) {
+            break
+        }
+
+        if (n % prime === 0) {
+            return [prime, ...primeFactors(n / prime)]
+        }
+    }
+
+    return []
+}
+
+function largestPrimeFactor(n: number): number {
+    return Math.max(...primeFactors(n)) //?
+}
+
+largestPrimeFactor(600851475143) //?
