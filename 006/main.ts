693a8002959a67adbc1242184c554a1aa2bdfc89
Nicole Watts <76132106+nicolecomputer@users.noreply.github.com>
2024-11-07 19:48:54 -0500

    problem 6

--- 006/main.ts
+++ 006/main.ts
@@ -1,7 +1,3 @@
-function square(n: number): number {
-    return Math.pow(n, 2)
-}
-
 function range(start: number, end: number): number[] {
     if (start > end) {
         return []
@@ -14,8 +10,37 @@ function range(start: number, end: number): number[] {
     ].map(i => i + start)
 }
 
+
+function sum(n: number, total: number | undefined): number {
+    return (total || 0) + n
+}
+
+function square(n: number): number {
+    return Math.pow(n, 2)
+}
+
+type Range = {
+    start: number,
+    end: number
+}
+
+function squareOfSum(r: Range): number {
+    return square(range(r.start, r.end).reduce(sum))
+}
+
+function sumOfSquares(r: Range): number {
+    return range(r.start, r.end).map(square).reduce(sum)
+}
+
+function difference<T>(fn1: (args: T) => number, fn2: (args: T) => number, args: T): number {
+    return fn1(args) - fn2(args)
+}
+
 function main() {
-    range(1, 10) //?
+    const result = difference(
+        squareOfSum, sumOfSquares,
+        { start: 1, end: 10 }) //?
+    console.log(result)
 }
 
 main();
